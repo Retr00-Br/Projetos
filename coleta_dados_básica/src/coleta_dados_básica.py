f31414908662dff3a6badd11b741a30a8d9dce80
Pedro Barbosa de Andrade <japingas@DESKTOP-9DDHTGH.localdomain>
2026-01-17 21:14:12 -0300

    Organiza projeto, adiciona src, README,requirements e notebooks

---
+++ coleta_dados_básica/src/coleta_dados_básica.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# URL de exemplo com TABELA HTML real
+url = "https://www.fdic.gov/resources/resolutions/bank-failures/failed-bank-list/"
+
+print("Requisitando página")
+response = requests.get(url)
+
+# Verifica se a página respondeu corretamente
+if response.status_code != 200:
+    raise Exception(f"Erro ao acessar a página: {response.status_code}")
+
+print("Página carregada com sucesso!")
+
+# Parser
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Localiza a tabela pelo seletor CSS
+tabela_html = soup.find("table")
+
+# Verifica se encontrou tabela
+if tabela_html is None:
+    raise Exception("Nenhuma tabela encontrada na página.")
+
+# Converte automaticamente com pandas
+df = pd.read_html(str(tabela_html))[0]
+
+print("Tabela extraída com sucesso!")
+print(df.head())
